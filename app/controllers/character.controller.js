b16ede5f102f23f95e2cd11748ce09083025e95e
Greg Satterlee <greg.satterlee@gmail.com>
2024-05-21 13:09:33 -0500

    changed recipeapi to storyapi

--- app/controllers/character.controller.js
+++ app/controllers/character.controller.js
@@ -15,3 +15,5 @@ exports.create = (character) => {
     console.log('>> Error while creating Character: ', err);
   });
 };
+
+
--- app/routes/character.routes.js
+++ app/routes/character.routes.js
@@ -14,3 +14,32 @@ module.exports = (app) => {
     );
 
 };
+
+exports.findAllForUser = (req, res) => {
+  const userId = req.params.userId;
+  Character.findAll({
+    where: { userId: userId },
+    include: [
+      {
+        model: StoryCharacter,
+        as: "storyCharacter",
+        required: false,
+      },
+    ],
+  })
+    .then((data) => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find Characters for user with id=${userId}.`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Error retrieving Characters for user with id=" + userId,
+      });
+    });
+};
