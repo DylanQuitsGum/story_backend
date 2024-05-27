5b8e8d1aeca403975ba1901f4f570833b3accb6f
Greg Satterlee <greg.satterlee@gmail.com>
2024-05-26 22:45:22 -0500

    Added reference to Cohere Added reference to uuid Added Story.routes.js Add CreateStory to interface to Cohere API

--- app/controllers/story.controller.js
+++ app/controllers/story.controller.js
@@ -2,6 +2,60 @@ const db = require("../models");
 const Story = db.story;
 const Op = db.Sequelize.Op;
 
+const { CohereClient } = require('cohere-ai');
+const {
+    v1: uuidv1,
+    v4: uuidv4,
+} = require('uuid');
+
+exports.createStory = async (req, res) => {
+
+  if(req.body.preamble == undefined){
+    const error = new Error("Preamble cannot be empty!");
+    error.statusCode = 400;
+    throw error;
+  }
+  if(req.body.prompt == undefined){
+    const error = new Error("Prompt cannot be empty!");
+    error.statusCode = 400;
+    throw error;
+  }
+
+  let title = req.body.title == undefined ? "" : req.body.title;
+  const conversationId = req.body.conversationId == undefined ? uuidv4() : req.body.conversationId;
+
+  let cohere = new CohereClient({
+    token: process.env.COHERE_KEY
+  });
+
+  const chat = await cohere.chat({
+    conversationId: conversationId,
+    preamble : req.body.preamble,
+    message: req.body.prompt
+  });
+
+  const storyBody = chat.text;
+
+  if(title == ''){
+    const storyTitleChat = await cohere.chat({
+      conversationId: conversationId,
+      message: 'What is the title of the story?',
+    });
+
+    title = storyTitleChat.text;
+  }
+
+  const story = {
+    story: storyBody,
+    conversationId: conversationId,
+    title: title
+  }
+
+  res.status(201).send({
+    response: story
+  })
+};
+
 // Create and Save a new Story
 exports.create = (req, res) => {
   // Validate request
---
+++ app/routes/story.routes.js
@@ -0,0 +1,25 @@
+module.exports = app => {
+    const stories = require("../controllers/story.controller.js");
+  
+    var router = require("express").Router();
+  
+    // Create a new Story
+    router.post("/", stories.createStory);
+  
+    // Retrieve all Stories
+    router.get("/", stories.findAll);
+  
+    // Retrieve a single Story with id
+    router.get("/:id", stories.findOne);
+  
+    // Update a Story with id
+    router.put("/:id", stories.update);
+  
+    // Delete a Story with id
+    router.delete("/:id", stories.delete);
+  
+    // Delete all Stories
+    router.delete("/", stories.deleteAll);
+  
+    app.use('/storyapi/story', router);
+  };
