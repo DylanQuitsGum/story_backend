1e06e41cb8af3da4e70bb738d5fd6b4b0862831d
bluegene92 <dattran10@gmail.com>
2024-06-04 17:05:49 -0500

    Resolve route

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,10 +1,12 @@
+const { isAuthorized } = require("../authentication/authentication");
+
 module.exports = (app) => {
   const User = require("../controllers/user.controller.js");
   const Story = require("../controllers/story.controller.js");
   var router = require("express").Router();
 
   router.post("/", User.create);
-  router.get("/:id/stories", Story.findAll);
-  router.get("/:id/stories/:storyId", Story.findOne);
+  router.get("/:id/stories", [isAuthorized], Story.findAll);
+  router.get("/:id/stories/:storyId", [isAuthorized], Story.findOne);
   app.use("/api/users", router);
 };
